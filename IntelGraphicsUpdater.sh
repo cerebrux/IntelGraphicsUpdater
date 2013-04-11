6a1633e7be7db10beaf5923434a6cd8484c693c6
aldo iljazi <aldo@linux.(none)>
2013-04-12 02:57:04 +0300

    added password prompt notification

--- IntelGraphicsUpdater.sh
+++ IntelGraphicsUpdater.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+notify-send -i $(pwd)/icons/password.png "Type your user password."
+sleep 2
 gksudo sh ./script.sh
--- script.sh
+++ script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+sleep 2
 if [ -f /etc/apt/sources.list.d/intellinuxgraphics.list ]
 then
-  notify-send -t 3 -i $(pwd)/icons/error.png "You have already installed the Intel graphics driver."
+  notify-send -i $(pwd)/icons/error.png "You have already installed the Intel graphics driver."
 else
-  notify-send -t 3 -i $(pwd)/icons/progress.png "The script is installing the latest Intel graphics driver."
+  notify-send -i $(pwd)/icons/progress.png "The script is installing the latest Intel graphics driver."
   echo "deb https://download.01.org/gfx/ubuntu/12.04/main Ubuntu 12.04 #Intel Graphics drivers" | sudo tee /etc/apt/sources.list.d/intellinuxgraphics.list
   wget --no-check-certificate https://download.01.org/gfx/RPM-GPG-KEY-ilg -O  /tmp/intelrepo.key
   sudo apt-key add /tmp/intelrepo.key
   sudo apt-get update
   sudo apt-get dist-upgrade
-  notify-send -t 3 -i $(pwd)/icons/success.png "The Intel graphics driver has been installed."
+  notify-send -i $(pwd)/icons/success.png "The Intel graphics driver has been installed."
 fi
