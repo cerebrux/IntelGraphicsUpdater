04d56ee63cbfa6d6a341dd7e7c9e8484cf5deb05
Salih Emin <salihemin@about.me>
2013-04-12 11:53:39 +0300

    Update License and Description
    Applied some 'best practices' on source code info
    Added license and some information. Needs some refining and description.

--- IntelGraphicsUpdater.sh
+++ IntelGraphicsUpdater.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#______________________________________________________________________________________________________
+#Name   : IntelGraphicsUpdater
+#Licence: ΜΙΤ (http://aldoiljazi.mit-license.org/)
+#Author : Aldo Iljazi
+#Email  :  (at)  
+#Date   : xx-xx-2013 (first release xx-xx-2013)
+#Version: x.x
+#System : Linux
+#WebSite: http://website.com
+#Source Code: Everything below comments is the sourcecode :)
+#Description:
+#        bla bla bla bla
+#______________________________________________________________________________________________________
 
 notify-send -i $(pwd)/icons/password.png "Type your user password."
 sleep 2
